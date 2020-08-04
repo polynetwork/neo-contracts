6a77608c996d313219dfaf1c00360c6c63ad5b85
John Wong <jwwwwong@gmail.com>
2020-08-04 16:16:10 +0800

    Fix tx args

--- Nep5ProxyPip1.cs
+++ Nep5ProxyPip1.cs
@@ -268,7 +268,7 @@ namespace Nep5Proxy
             }
 
             // construct args for proxy contract on target chain
-            var inputArgs = SerializeArgs(fromAssetHash, toAssetHash, toAddress, amount, feeAmount, feeAddress);
+            var inputArgs = SerializeArgs(fromAssetHash, toAssetHash, toAddress, amount, feeAmount, feeAddress, fromAddress);
             // construct params for CCMC
             var param = new object[] { toChainId, targetProxyHash, "unlock", inputArgs };
             // dynamic call CCMC
@@ -496,7 +496,7 @@ namespace Nep5Proxy
             return new object[] { buffer.Range(offset, count), offset + count };
         }
 
-        private static byte[] SerializeArgs(byte[] fromAssetHash, byte[] toAssetHash, byte[] toAddress, BigInteger amount, BigInteger feeAmount, byte[] feeAddress)
+        private static byte[] SerializeArgs(byte[] fromAssetHash, byte[] toAssetHash, byte[] toAddress, BigInteger amount, BigInteger feeAmount, byte[] feeAddress, byte[] fromAddress)
         {
             var buffer = new byte[] { };
             buffer = WriteVarBytes(fromAssetHash, buffer);
@@ -505,6 +505,7 @@ namespace Nep5Proxy
             buffer = WriteUint255(amount, buffer);
             buffer = WriteUint255(feeAmount, buffer);
             buffer = WriteVarBytes(feeAddress, buffer);
+            buffer = WriteVarBytes(fromAddress, buffer);
             return buffer;
         }
 
